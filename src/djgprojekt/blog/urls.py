a161c53719b482c977cc76ba5a838d6e27041328
jabukasiga <underadmin@o2.pl>
2021-06-23 20:10:36 +0200

    moj drugi commit

--- src/djgprojekt/blog/urls.py
+++ src/djgprojekt/blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.post_list, name='post_list')
+	path('blabla', views.post_list, name='post_list'),
+	path('', views.strona2, name='strona2'),
+	path('szansa/', views.last_chance, name='chance')
 
 ]
--- src/djgprojekt/blog/views.py
+++ src/djgprojekt/blog/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 def post_list(request):
 	return render(request, 'blog/post_list.html', {})
 
+def strona2(request):
+	return render(request, 'blog/strona2.html', {})
+
+def last_chance(request):
+	return render(request, 'blog/ostatnia_szansa.html', {})
+
 # Create your views here.
